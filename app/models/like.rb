eb660ba934a97e2cd9295b0bed62cdac64fe63f4
oguntayomicheal <oguntayomicheal@gmail.com>
2023-07-13 00:06:18 +0100

    Fix linters error

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,13 +1,12 @@
 class Like < ApplicationRecord
-    belongs_to :author, class_name: 'User', foreign_key: :author_id
-    belongs_to :post
+  belongs_to :author, class_name: 'User', foreign_key: :author_id
+  belongs_to :post
 
-    after_save :update_likes_counter
+  after_save :update_likes_counter
 
+  def update_likes_counter
+    post.update(likes_counter: post.likes_counter.to_i + 1)
+  end
 
-    def update_likes_counter
-        post.update(likes_counter: post.likes_counter.to_i + 1)
-    end
-
-    private :update_likes_counter
+  private :update_likes_counter
 end
