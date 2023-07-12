04666eec070f3b135bad42b855267d340b05629f
oguntayomicheal <oguntayomicheal@gmail.com>
2023-07-12 22:52:42 +0100

    Add comments methods

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,12 @@
 class Comment < ApplicationRecord
     belongs_to :author, class_name: 'User', foreign_key: :author_id
     belongs_to :post
+
+    after_save :update_comments_counter
+
+    def update_comments_counter
+      post.update(comments_counter: author.comments_counter + 1)
+    end
+  
+    private :update_comments_counter
 end
