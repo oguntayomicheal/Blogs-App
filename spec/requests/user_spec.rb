2508a0f02c68c573382106f4477eeaf1c5655525
oguntayomicheal <oguntayomicheal@gmail.com>
2023-07-18 21:44:02 +0100

    Add spec for user/show action

--- spec/requests/user_spec.rb
+++ spec/requests/user_spec.rb
@@ -7,15 +7,31 @@ RSpec.describe UsersController, type: :request do
       expect(response).to be_successful
     end
 
-    it 'renders the index template of views/users' do
+    it 'renders the index template of users' do
       get '/users/'
       expect(response).to render_template(:index)
     end
-  end
-  it 'includes the correct placeholder text' do
-    get '/users/'
-    expect(response.body).to include('Here is a list of all users')
+
+    it 'includes the correct placeholder text' do
+      get '/users/'
+      expect(response.body).to include('Here is a list of all users')
+    end
   end
 
+  describe 'GET /users/show' do
+    it 'returns a success response' do
+      get '/users/45'
+      expect(response).to be_successful
+    end
 
+    it 'renders the show template' do
+      get '/users/45'
+      expect(response).to render_template(:show)
+    end
+
+    it 'includes the correct placeholder text' do
+      get '/users/45'
+      expect(response.body).to include('Here are the details of a particular user')
+    end
+  end
 end
