842e03f9b63cbf959c23bb23ae60e35993526dfe
oguntayomicheal <oguntayomicheal@gmail.com>
2023-07-18 03:47:48 +0100

    Add routes and controller for the posts

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@
 
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :users
+  resources :users do
+    resources :posts
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
