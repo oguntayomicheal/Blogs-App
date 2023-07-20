6e2123fa5e03dda292e1e286eafde47bf495a51a
oguntayomicheal <oguntayomicheal@gmail.com>
2023-07-20 13:22:07 +0100

    Update post index.html to show only 5 recent comments

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,6 @@
 class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
-    @user_posts = Post.where(author_id: @user.id)
-    @post_comments = Comment.where(post_id: @user_posts.ids)
   end
 
   def show
