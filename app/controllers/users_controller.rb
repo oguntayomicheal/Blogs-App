a3b80f6ff5eae324e559cdef40d7be5f714eb11e
oguntayomicheal <oguntayomicheal@gmail.com>
2023-07-26 15:06:55 +0100

    Run migrations for confirmable

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
   def index
     @all_users = User.all
   end
--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,5 @@ Rails.application.routes.draw do
   end
   # Defines the root path route ("/")
   # root "articles#index"
+  root 'users#index', as: :authenticated_root
 end
